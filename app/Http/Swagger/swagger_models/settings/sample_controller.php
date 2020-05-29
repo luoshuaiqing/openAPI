7b917ec9badc8ca3762deb6dcd1f9c50e8541128
luoshuaiqing <shuaiqin@usc.edu>
2020-05-29 15:17:36 -0700

    test

--- app/Http/Swagger/swagger_models/settings/sample_controller.php
+++ app/Http/Swagger/swagger_models/settings/sample_controller.php
@@ -7,6 +7,11 @@ class SampleController //This is a sample laravel Controller
      *     tags={"greeting"},
      *     summary="Returns a Sample API response",
      *     description="A sample greeting to test out the API",
+     *     @OA\Parameter(
+     *          name="id",
+     *          description="Project id",
+     *          in="path",
+     *     ),
      *     operationId="sampleFunctionWithDoc",
      *     @OA\Response(
      *         response="default",
@@ -14,7 +19,38 @@ class SampleController //This is a sample laravel Controller
      *     )
      * )
      */
-    public function sampleFunctionWithDoc()
+    public function sampleFunctionWithDoc(Reqhest $request)
     {
+
     }
+/**
+ * @OA\Get(
+ *      path="/projects/{id}",
+ *      operationId="getProjectById",
+ *      tags={"Projects"},
+ *      summary="Get project information",
+ *      description="Returns project data",
+ *      @OA\Parameter(
+ *          name="id",
+ *          description="Project id",
+ *          required=true,
+ *          in="path",
+ *          @OA\Schema(
+ *              type="integer"
+ *          )
+ *      ),
+ *      @OA\Response(
+ *          response=200,
+ *          description="successful operation"
+ *       ),
+ *      @OA\Response(response=400, description="Bad request"),
+ *      @OA\Response(response=404, description="Resource Not Found"),
+ *      security={
+ *         {
+ *             "oauth2_security_example": {"write:projects", "read:projects"}
+ *         }
+ *     },
+ * )
+ */
+
 }
